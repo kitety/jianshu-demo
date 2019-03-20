5e1cf851f4cf6b82e878a8ce9b543017f07dec91
kitety <aizaizuori@foxmail.com>
2019-03-20 22:08:20 +0800

    header组件基本

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {GlobalStyle} from './style';
+import Header from './common/header/Header'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <GlobalStyle/>
-        <header className="App-header cell">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <GlobalStyle/>
+          <Header/>
+        </div>
     );
   }
 }
---
+++ src/common/header/style.js
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+import logoPic from "../../statics/logo.png";
+
+export const HeaderWrapper = styled.div`
+  height: 56px;
+  border-bottom: 1px solid #f0f0f0;
+  position: relative;
+`;
+export const Logo = styled.a.attrs({
+  href: "/"
+})`
+  height: 56px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100px;
+  background: url(${logoPic});
+  background-size: contain;
+`;
+export const Nav = styled.div`
+  width: 960px;
+  height: 100%;
+  padding-right: 70px;
+  margin: 0 auto;
+  box-sizing: border-box;
+`;
+export const NavItem = styled.div`
+  line-height: 56px;
+  padding: 0 15px;
+  font-size: 17px;
+  color: #333;
+  &.left {
+    float: left;
+  }
+  &.right {
+    color: #969696;
+    float: right;
+  }
+  &.active {
+    color: #ea6f5a;
+  }
+`;
+export const NavSearch = styled.input.attrs({
+  placeholder: "搜索"
+})`
+  width: 240px;
+  height: 38px;
+  border: none;
+  outline: none;
+  border-radius: 19px;
+  margin-top: 9px;
+  padding: 0 20px;
+  box-sizing: border-box;
+  background: #eee;
+  font-size: 14px;
+  margin-left: 20px;
+  &::placeholder {
+    color: #999;
+  }
+`;
+export const Addition = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  height: 56px;
+`;
+export const Button = styled.div`
+  float: right;
+  line-height: 38px;
+  border-radius: 19px;
+  margin-top: 9px;
+  border: 1px solid #ea6f5a;
+  margin-right: 20px;
+  padding: 0 20px;
+  font-size: 15px;
+  &.reg {
+    color: #ea6f5a;
+  }
+  &.writting {
+    background: #ea6f5a;
+    color: #fff;
+  }
+`;
